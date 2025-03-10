572e9d49df035022b6a6106d6def48ecf960edc6
Prashantha-Nayak <prashanthnayak2105@gmail.com>
2025-03-10 22:43:37 +0530

    Added React frontend and Spring Boot backend

---
+++ AI-Email-Assistant/src/test/java/com/Email/writer/AiEmailAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Email.writer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiEmailAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
